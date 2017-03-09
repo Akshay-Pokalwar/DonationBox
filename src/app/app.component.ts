f12d621f0f6017487f5654d77f28d3e981e04ca7
Ketaki Patwari <patwariketaki@gmail.com>
2017-03-09 17:27:37 +0530

    page3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
-//import { Page2 } from '../pages/page2/page2';
+import { Page2 } from '../pages/page2/page2';
+import { Page3 } from '../pages/page3/page3';
 
 
 @Component({
@@ -12,7 +13,7 @@ import { Page1 } from '../pages/page1/page1';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any = Page3;
 
   pages: Array<{title: string, component: any}>;
 
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Page One', component: Page1 },
-      // { title: 'Page Two', component: Page2 }
+      { title: 'Page Two', component: Page2 },
+      { title: 'Page Three', component: Page3 }
     ];
 
   }
