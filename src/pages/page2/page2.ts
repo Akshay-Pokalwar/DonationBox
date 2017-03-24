f22b113355a47fa01590c708bbb20c6be66569b2
Akshay Pokalwar <akshaypklwr@gmail.com>
2017-03-24 16:54:04 +0530

    page for adding dynamic list

--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
+import { AngularFire,FirebaseListObservable} from 'angularfire2';
 import { NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 import { LoginPage } from '../login/login';
 
 @Component({
@@ -8,25 +9,51 @@ import { LoginPage } from '../login/login';
   templateUrl: 'page2.html'
 })
 export class Page2 {
+  public name:string='';
+  public mfg_date:string='';
+  public exp_date:string='';
+  public address:string='';
+  public phone:string='';
+  public email:string='';
+  
+  
   selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    
-
-    // Let's populate this page with some filler content for funzies
-    
+  
+lists:FirebaseListObservable<any[]>;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public af:AngularFire,public alertCtrl: AlertController) {
+  this.lists = af.database.list('/lists',{preserveSnapshot:true});
+}
+  add()
+    {
+        this.lists.push({
+      'name':this.name,
+      'mfg_date':this.mfg_date,
+      'exp_date':this.exp_date,
+      'addr':this.address,
+      contact:{
+        'phone':this.phone,
+        'email':this.email
+      }
+    }).then(
+      (res)=>{console.log(res)
+      this.showAlert('Success!','Record inserted');},
+      (err:any)=>{console.error(err)
+    this.showAlert(err.code,err.message);  
+    }
+    );
+     }
+showAlert(x,y) {
+    let alert = this.alertCtrl.create({
+      title: x,
+      subTitle: y,
+      buttons: ['OK']
+    });
+    alert.present();
   }
+ 
 public logout()
   {
     this.navCtrl.setRoot(LoginPage);
   }
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(Page2, {
-      item: item
-    });
-  }
+  
 }
