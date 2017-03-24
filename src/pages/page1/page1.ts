cd1e5e79d7152a2c5b39fa26f86a40cafef1045f
Akshay Pokalwar <akshaypklwr@gmail.com>
2017-03-24 15:36:24 +0530

    few changes

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -8,33 +8,39 @@ import { LoginPage } from '../login/login';
   templateUrl: 'page1.html'
 })
 export class Page1 {
-  public myItem:any='';
-  public myI:any;
-  public myIt:any;
+  public items:any='';
   
   // item: FirebaseObjectObservable<any[]>;
   lists:FirebaseListObservable<any[]>;
   constructor(public navCtrl: NavController, public af:AngularFire) {
+  
     this.lists = af.database.list('/lists',{preserveSnapshot:true});
-this.lists.subscribe((result:any)=>{
-  result.forEach(result => {
-    // console.log(result.key)
-    //   console.log(result.val())
-      this.myItem=this.myItem+result.key+':';
-      this.myItem+=result.val()+'\n';
-      
+    
+    this.lists.subscribe((result:any)=>{
+      this.items = result;
+      console.log(this.items)
     });
-      });
-//     this.item = af.dat   abase.object('/item');
-//     this.item.subscribe((result:any)=>{
-//       console.log(result);
-//       this.myItem = result.$key;
-//       this.myItem = result.$value;
-// });   
+
   }
   public login()
   {
     this.navCtrl.setRoot(LoginPage);
+  }
+  public add()
+  {
+    this.lists.push({
+      'name':'Crocin',
+      'mfg_date':'11/2015',
+      'exp_date':'12/2020',
+      'addr':'Nanded',
+      contact:{
+        'phone':'8888004844',
+        'email':'akshaypklwr@gmail.com'
+      }
+    }).then(
+      (res)=>{console.log(res)},
+      (err)=>{console.error(err)}
+    );
   }
 
 }
